e2e111704e620f2fc4112928a64377afb2695aab
Suchanat-nut <78488006+nutkung1@users.noreply.github.com>
2024-02-02 02:37:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import pymysql
 pymysql.install_as_MySQLdb()
 st.title("Hello")
 # Initialize connection.
-
+conn = st.connection('mysql', type='sql')
 tab1, tab2, tab3 = st.tabs(["Farmer", "Cultivated_areas", "Fertilizer"])
 option = st.sidebar.selectbox("Select an Operations", ("สร้าง", "อ่าน", "อัพเดท", "ลบ"))
 #farmer tab
 with tab1:
-    conn = st.connection('mysql', type='sql')
 
         # Perform query.
     df = conn.query('select * from farmer;', ttl=600)
